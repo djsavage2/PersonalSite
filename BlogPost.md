2fa5f4590a544d94b9c395ae4a75974bcfaf6f70
Savage <djsavage2@bsu.edu>
2021-01-20 08:08:37 -0500

    add Navbar.svelte, tinker with App.svelte

---
+++ BlogPost.md
@@ -0,0 +1,7 @@
+---
+title: Sample BlogPost
+description: "sample description"
+publish-date: 01-20-2021
+---
+
+Sample Blog Post
--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,30 +1,31 @@
 <script lang="ts">
+	import Navbar from "./Navbar.svelte";
 	export let name: string;
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<Navbar/>
+<body>
+	<h1>{name}</h1>
+	<p> Test Code Color</p>
+</body>
 
 <style>
-	main {
+	body {
+		background-color: #000000;
 		text-align: center;
 		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
 
 	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
+		color:#CCCCCC;
 		font-size: 4em;
 		font-weight: 100;
+		font-family: Noto Mono;
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
+	p {
+		color: #00FF00;
+		font-family: Noto Mono;
 	}
 </style>
